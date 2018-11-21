bc9ac05b1c8d8f0439f9bd2e9ef460b6edf21bf3
Ganemo <ganemogiannis@knights.ucf.edu>
2018-11-20 20:31:35 -0800

    Small Changes

--- src/RoutingServer.java
+++ src/RoutingServer.java
@@ -20,7 +20,7 @@ public class RoutingServer {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
+            t.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
