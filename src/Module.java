67bd7c6e5b80408ac73c933bb20ab5487e5c5233
Ganemo <ganemogiannis@knights.ucf.edu>
2018-11-24 13:11:54 -0800

    More logging

--- src/Module.java
+++ src/Module.java
@@ -2,12 +2,17 @@
 public class Module {
 
     public static void main(String[] args) {
+    	System.out.println("Starting Server");
+    	
     	RoutingServer Server = new RoutingServer();
     	
+    	System.out.println("Starting Listener");
     	try {
-    	UDPSocketListener Listener = new UDPSocketListener(Server);
+    		UDPSocketListener Listener = new UDPSocketListener(Server);
     	} catch (Exception e) {
+    		System.out.println("Listener Failed");
     		e.printStackTrace();
     	}
+    	System.out.println("Listener Success!");
     }
 }
