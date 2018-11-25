bb1dd509c1819870fd8000ce01eb80f7fd776014
Ganemo <ganemogiannis@knights.ucf.edu>
2018-11-24 16:19:02 -0800

    Uncommenting main

--- src/Module.java
+++ src/Module.java
@@ -4,28 +4,28 @@ public class Module {
 	static UDPSocketListener Listener;
 	static RoutingServer Server;
 	
-    public static void main(String[] args) {
-    	
-    	Runtime.getRuntime().addShutdownHook(new Thread()
-    	{
-    	    @Override
-    	    public void run()
-    	    {
-    	    	Listener.stop();
-    	    	Server.KickAllPlayers();
-    	    }
-    	});
-    	
-    	System.out.println("Starting Server");
-    	Server = new RoutingServer();
-    	
-    	try {
-    		System.out.println("Starting Listener");
-    		Listener = new UDPSocketListener(Server);
-    		Listener.start();
-    	} catch (Exception e) {
-    		System.out.println("Listener Failed");
-    		e.printStackTrace();
-    	}
-    }
+//    public static void main(String[] args) {
+//    	
+//    	Runtime.getRuntime().addShutdownHook(new Thread()
+//    	{
+//    	    @Override
+//    	    public void run()
+//    	    {
+//    	    	Listener.stop();
+//    	    	Server.KickAllPlayers();
+//    	    }
+//    	});
+//    	
+//    	System.out.println("Starting Server");
+//    	Server = new RoutingServer();
+//    	
+//    	try {
+//    		System.out.println("Starting Listener");
+//    		Listener = new UDPSocketListener(Server);
+//    		Listener.start();
+//    	} catch (Exception e) {
+//    		System.out.println("Listener Failed");
+//    		e.printStackTrace();
+//    	}
+//    }
 }
